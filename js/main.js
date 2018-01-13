7d47ee1e75db87b381ad0ed505491280a483e43b
Srkinator <srki2207@gmail.com>
2018-01-13 16:05:59 +0100

    List of Candidates Page done

---
+++ js/main.js
@@ -0,0 +1,29 @@
+(loadCandidates =()=>{
+    fetch("http://localhost:3333/api/candidates")
+    .then(result => result.json())
+    .then(result => {
+        result.forEach((candidate,i)=>{
+            let cardContainer = document.createElement("div");
+            let avatar = document.createElement("img");
+            let email = document.createElement("p");
+            let name = document.createElement("h4");
+
+            name.textContent = candidate.name;
+            email.textContent = candidate.email;
+            if(candidate.avatar.length>1){
+                avatar.setAttribute("src", candidate.avatar);
+            }
+            else {
+                avatar.setAttribute("src", "http://via.placeholder.com/128x128");
+            }
+            cardContainer.setAttribute("class", "candidate-card col-xl-4 col-lg-4 col-md-6 col-sm-6 col-xs-12");
+
+            let card = document.getElementsByClassName("candidate-container")[0];
+            cardContainer.appendChild(avatar);
+            cardContainer.appendChild(name);
+            cardContainer.appendChild(email);
+            card.appendChild(cardContainer);
+
+        })
+    })
+})();
